ba5fbf88320c024d5028d86a8585e27608d48a4e
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2023-08-02 17:50:17 +0300

    maxwelld interface prototype

---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+README = open("README.md").read()
+setup(
+    name="maxwelld",
+    version="0.0.1",
+    description="docker compose testing env orchestrator",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Yuriy Sagitov",
+    author_email="pro100.ko10ok@gmail.com",
+    python_requires=">=3.10.0",
+    url="https://github.com/ko10ok/maxwelld",
+    license="Apache-2.0",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
