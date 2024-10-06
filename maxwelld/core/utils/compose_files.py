869b325e849876f27153907376031bd11ba0ed18
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2024-10-06 14:33:01 +0700

    fix deps with override service

--- maxwelld/core/utils/compose_files.py
+++ maxwelld/core/utils/compose_files.py
@@ -339,8 +339,10 @@ def parse_docker_compose(file_paths):
         dc_cfg = read_dc_file(file_path)
         services = dc_cfg.get('services', {})
         for service_name, service_data in services.items():
-            dependencies = service_data.get('depends_on', [])
-            services_dict[service_name] = dependencies
+            service_deps = set(services_dict.get(service_name, []))
+            current_file_service_deps = set(service_data.get('depends_on', []))
+
+            services_dict[service_name] = list(service_deps | current_file_service_deps)
 
     return services_dict
 
