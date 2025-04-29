16fa6ecd0bdaa0173f359c6f718b934c479c4f3b
Юрий Сагитов <yu.sagitov@2gis.ru>
2025-04-29 08:56:35 +0300

    fix test

--- tests/schemas/service.py
+++ tests/schemas/service.py
@@ -2,7 +2,7 @@ import string
 
 from d42 import schema
 
-start_letter = string.ascii_letters + string.digits + '_-'
+start_letter = string.ascii_letters + string.digits + '_'
 end_letter = string.ascii_letters + string.digits + '_-'
 
 ServiceNameSchema = schema.str.regex(rf'[{start_letter}]+[{end_letter}]+')
